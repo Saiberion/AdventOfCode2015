c625466d6669640aa0eaf3ab7f77cb7166cba1c0
Saiberion <saiberion@web.de>
2019-03-14 10:46:50 +0100

    Day 4 completely solved

---
+++ Days/Day04.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days
+{
+    public class Day04: Day
+    {
+        public Day04()
+        {
+            Part1Text = "Lowest AdventCoin Hash (5 zeroes)";
+            Part2Text = "Lowest AdventCoin Hash (6 zeroes)";
+
+            Load("inputs/day04.txt");
+        }
+
+        override public void Solve()
+        {
+            MD5Managed md5 = new MD5Managed();
+            string key = Input[0];
+            byte[] hash;
+
+            int index = 1;
+            while(true)
+            {
+                hash = md5.ComputeHash(Encoding.ASCII.GetBytes(key + index));
+
+                if ((hash[0] == 0) && (hash[1] == 0) && (hash[2] <= 0x0f))
+                {
+                    Part1Solution = index.ToString();
+                    break;
+                }
+
+                index++;
+            }
+
+            while (true)
+            {
+                hash = md5.ComputeHash(Encoding.ASCII.GetBytes(key + index));
+
+                if ((hash[0] == 0) && (hash[1] == 0) && (hash[2] == 0))
+                {
+                    Part2Solution = index.ToString();
+                    break;
+                }
+
+                index++;
+            }
+        }
+    }
+}
