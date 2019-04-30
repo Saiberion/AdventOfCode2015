3d9b50fbd55a9bdabe214762cf07d8301708fcf0
Saiberion <saiberion@web.de>
2019-04-30 14:23:51 +0200

    Day 19 Part 1 solved

---
+++ Days/Day19.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Days
+{
+    class ReplacementRule
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+
+    public class Day19: Day
+    {
+        public Day19()
+        {
+            Part1Text = "Unique molecules";
+            Part2Text = "";
+
+            Load("inputs/day19.txt");
+        }
+
+        override public void Solve()
+        {
+            string startingMolecule = "";
+            List<ReplacementRule> replacementRules = new List<ReplacementRule>();
+            HashSet<string> uniqueMolecules = new HashSet<string>();
+
+            foreach (string s in Input)
+            {
+                if (s.Contains("=>"))
+                {
+                    // Replacement rule
+                    string[] splitted = s.Split(new char[] { ' ', '=', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                    replacementRules.Add(new ReplacementRule() { From = splitted[0], To = splitted[1] });
+                }
+                else if (s.Length > 0)
+                {
+                    // starting molecule
+                    startingMolecule = s;
+                }
+            }
+
+            foreach(ReplacementRule r in replacementRules)
+            {
+                int searchIndex = 0;
+                int foundIndex;
+
+                while((foundIndex = startingMolecule.IndexOf(r.From, searchIndex)) >= 0)
+                {
+                    string n = startingMolecule.Substring(0, foundIndex) + r.To + startingMolecule.Substring(foundIndex + r.From.Length);
+                    uniqueMolecules.Add(n);
+                    searchIndex = foundIndex + r.From.Length;
+                }
+            }
+
+            Part1Solution = uniqueMolecules.Count.ToString();
+        }
+    }
+}
