78f6089c9ac2a2c9aa438f1c3b4ab9f540b95508
Saiberion <saiberion@web.de>
2019-05-27 11:26:11 +0200

    Completed Day 25

---
+++ Days/Day25.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Days
+{
+    public class Day25: Day
+    {
+        public Day25()
+        {
+            Part1Text = "Found code";
+            Part2Text = "Weather Machine";
+
+            Load("inputs/day25.txt");
+        }
+
+        override public void Solve()
+        {
+            int row = 2981;
+            int col = 3075;
+            int r = 1, c = 1;
+            int index = 1;
+            int maxrow = 1;
+            long code = 20151125;
+
+            while(!((r == row) && (c == col)))
+            {
+                r--;
+                c++;
+                if (r == 0)
+                {
+                    c = 1;
+                    r = ++maxrow;
+                }
+                index++;
+                code *= 252533;
+                code %= 33554393;
+            }
+
+            Part1Solution = code.ToString();
+            Part2Solution = "Active";
+        }
+    }
+}
